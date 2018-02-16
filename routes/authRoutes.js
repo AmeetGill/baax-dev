06aa6c97929c5d22cbfc88191254cdf4bfd0d15b
AmeetGill <aameetpal_be15@thapar.edu>
2018-02-16 23:59:49 +0530

    added more featues

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,17 @@ module.exports = (app) => {
   app.get('/auth/google/callback',
     passport.authenticate('google')
   );
+
+  app.get('/api/current_user',(req,res)=>{
+    res.send(req.user);
+  });
+
+  app.get('/auth/facebook',passport.authenticate('facebook',{
+      scope:['public_profile']
+    })
+  );
+
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook')
+  );
 };
--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,20 @@
- const passport = require('passport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
+
+passport.serializeUser((user,done)=>{
+  done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+  User.findById(id)
+    .then((user =>{
+      done(null,user);
+    }));
+});
 
 passport.use(
   new GoogleStrategy({
@@ -15,11 +27,39 @@ passport.use(
       .then((existingUser) => {
         if(existingUser){
             // user already exist in the data base
+            done(null,existingUser);
         }
         else{
-            new User({googleId:profile.id}).save();
+            new User({googleId:profile.id})
+              .save()
+              .then(user => done(null,user));
         }
       })
 
+  })
+);
+
+passport.use(
+  new FacebookStrategy({
+    clientID:keys.facebookClientID,
+    clientSecret:keys.facebookClientSecret,
+    callbackURL:"http://localhost:5000/auth/facebook/callback"
+  },
+  (accessToken,refreshToken,profile,done) => {
+    console.log(profile);
+    User.findOne({facebookId:profile.id})
+      .then((existingUser) => {
+        if(existingUser){
+          done(null,existingUser);
+        }
+        else{
+          console.log(profile);
+          new User({facebookId:profile.id})
+            .save()
+            .then((user) => {
+              done(null,user)
+            });
+        }
+      })
   })
 );
