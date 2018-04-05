a45a8a43d3ba6ed67355de29cefda1538391f322
AmeetGill <aameetpal_be15@thapar.edu>
2018-04-06 00:36:05 +0530

    fix bugs

--- config/prod.js
+++ config/prod.js
@@ -4,7 +4,9 @@ module.exports = {
   facebookClientID : process.env.FACEBOOK_CLIENT_ID,
   facebookClientSecret : process.env.FACEBOOK_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey : process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey : process.env.STRIPE_SECRET_KEY
 }
 
 // hey this ust to coomit
